bf21ba86edd36618b5504862dc363ee1dfdd0e20
Vinícius Melo Oliveira <viniciusmelo241@gmail.com>
2021-09-16 20:16:13 -0300

    First commit

---
+++ src/styles/index.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle  } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    @import '~antd/dist/antd.css';
+    
+    *, :after, :before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body {
+        height: 100%;
+    }
+
+    :root {
+        --primary-color: #2563EB;
+        --secondary-color: #1E3A8A;
+        --tertiary-color: #1F2937;
+        --primary-bg-color: #1D2633;
+        --secondary-bg-color: #273345;
+        --primary-font-color: #18181B;
+        --secondary-font-color: #52525B;
+        --success: #54C112;
+        --warning: #FFC107;
+        --error: #EC2323;
+        --secondary: #E2E2E2;
+        --bg-color: #E5E5E5;
+
+        font-size: 60%;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
